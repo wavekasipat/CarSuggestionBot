308e92f93dce5a961be2114e2b9b6be904df26b5
wavekasipat <wavekasipat@gmail.com>
2018-05-19 10:44:29 +0700

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=carsuggestionbot-Web-Deploy.pubxml -p:Password=RsrHhoib49NcMHifhzLi7TRSiK6HEzQ4ojgMskg03Lr4bdMj6Ff3ZsefB5WE
+
